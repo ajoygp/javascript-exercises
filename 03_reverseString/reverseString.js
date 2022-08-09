8965b8e8917ac048b7bbf193e4ecb56cd472893a
ajoygp <despajoy@gmail.com>
2022-08-09 22:58:02 +0530

    Complete excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
-};
+const reverseString = function(text) {
+    let ret= "";
+    for(let i =0;  i<text.length;++i){
+        ret=text.charAt(i)+ret;
+}
+return (ret);
+}
 
 // Do not edit below this line
 module.exports = reverseString;
