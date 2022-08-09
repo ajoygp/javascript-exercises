00bf6c1bf30661668823808934efc095f1d1045b
ajoygp <despajoy@gmail.com>
2022-08-09 23:16:56 +0530

    Complete excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(typeof(a)!='number'|| typeof(b)!='number' ){
+        return('ERROR');
+    }
+
+    if(a<0 || b<0 ){
+        return('ERROR');
+    }
+
+    startint= a<b?a:b;
+    endint= a<b?b:a;
+    sum=0;
+    for(let i=startint;i<=endint;i++){
+        sum+=i;
+    }
+    return(sum);
+
 
 };
 
