3a9ddf8358f4adf23ee398eb278c05f4703beabb
ajoygp <despajoy@gmail.com>
2022-08-09 22:58:46 +0530

    Complete excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(list,...remove) {
+    for(let i=0;i<remove.length;i++){
+        removeIndex=list.indexOf(remove[i])
+        if(removeIndex<0){
+            continue;
+        }
+        list.splice(removeIndex,1);
+    }
+    return (list);
 
 };
 
