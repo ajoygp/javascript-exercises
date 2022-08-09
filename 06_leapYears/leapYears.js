01e9ffe4d5840b9dc14a0e5a3d2671451301a120
ajoygp <despajoy@gmail.com>
2022-08-10 00:06:35 +0530

    improved  excersie 6 solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,6 @@
 const leapYears = function(year) {
 
-    if(year%4==0){
-        if(year%100==0){
-            return(year%400==0?true:false);
-        }
-         
-        else{
-            return(true);
-        }
-
-    }
-    else{
-            return(false);
-        }
+    return(year % 4 == 0 && (year % 100 !== 0 || year % 400 ==0 ) ) 
 }
 
 
