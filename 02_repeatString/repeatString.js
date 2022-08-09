3294125a80b0cda0f69be66e022a6669aa205686
ajoygp <despajoy@gmail.com>
2022-08-09 22:57:40 +0530

    Complete excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(text,times) {
 
+    if(times===undefined){
+        return (text.repeat(Math.floor(Math.random()*10)))
+    }
+    if(!text || times==0){
+        return ('')
+    }
+
+    if (times<0){
+        return ("ERROR")
+    }
+    return (text.repeat(times))
+
+    
 };
 
 // Do not edit below this line
