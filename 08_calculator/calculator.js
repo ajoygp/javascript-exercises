b023a22c3aefc94667c135fce96907c58fc1ec47
ajoygp <despajoy@gmail.com>
2022-08-21 14:38:14 +0530

    finish calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	return nums.reduce((total,num)=>total+num,0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((product,num)=>product*num,1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let fact=1;
+  for(let i=1;i<n+1;i++){
+    fact*=i;
+  }
+  return (fact);
 };
 
 // Do not edit below this line
