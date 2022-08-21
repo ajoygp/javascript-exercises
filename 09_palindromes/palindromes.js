74afb0d73744de13da2f0c6ffb9677964b5233d7
ajoygp <despajoy@gmail.com>
2022-08-21 15:38:35 +0530

    Solve 09 panlindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str=str.replace(/[^a-zA-Z ]/g,"").toLowerCase().replaceAll(" ","");
+    rev=str.split("").reverse().join("").replaceAll(" ","");
+    return (rev==str)
 };
 
 // Do not edit below this line
