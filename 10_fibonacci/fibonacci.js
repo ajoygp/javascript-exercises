d74221a36af38e88fd815250925c4eeb3efe36f8
ajoygp <despajoy@gmail.com>
2022-08-21 17:00:17 +0530

    Solve 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if(typeof(n)==Text){
+        if (!n.match(/^[0-9]+$/)  ){
 
+            return("OOPS");
+        }
+    }
+
+    if(n<0 ){
+        return("OOPS");
+    }
+
+    let ret=1;
+    let prev=0;
+
+    for(let i=0;i<n-1;i++){
+        let temp=ret;
+        ret=ret+prev;
+        prev=temp;
+    }
+    return (ret);
 };
 
 // Do not edit below this line
