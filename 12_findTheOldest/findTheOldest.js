c80011a6c1e51df1ae97fc843153c8c38228273e
ajoygp <despajoy@gmail.com>
2022-08-21 18:12:35 +0530

    Solve 12 findtheoldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let findAge=function (person){
+        yearOfDeath=person.yearOfDeath? person.yearOfDeath:new Date().getFullYear()
+        return (yearOfDeath- person.yearOfBirth )
+    }  
+    return people.sort((a,b)=>findAge(b)-findAge(a))[0];
+    
+     
 };
 
 // Do not edit below this line
